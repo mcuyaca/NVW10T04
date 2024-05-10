17f359c401547ae590b6c70d3fc73756acee559d
mcuya.ca <mcuya.ca@gmail.com>
2024-05-10 12:34:41 -0500

    add styles and page example

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,10 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./scripts/counter.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+  <a href="./src/page/example.html"  > Ir a btn example </a>
+ <button class="button"> Hola </button>
+ <button class="button button--secondary"> Hola </button>
+ <button class="button button--outline" > Hola </button>
   </div>
 `;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
