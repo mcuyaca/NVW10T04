b6ad85feed9f01227a9656a0d6802251d3f52711
OliverCallas <A18101119@idat.edu.pe>
2024-05-11 07:49:33 -0500

    Second Update 1.1

--- src/scripts/monitor.ts
+++ src/scripts/monitor.ts
@@ -1,7 +1,12 @@
-import { fromEvent } from 'rxjs';
-import { throttleTime, map } from 'rxjs/operators';
+import { fromEvent, merge } from 'rxjs';
+import { throttleTime, map, scan, count } from 'rxjs/operators';
 
 const eyeElements = document.querySelectorAll('.eye');
+const position = document.querySelector('#position');
+const clicks = document.querySelector('#clicks');
+const copia = document.querySelector('#copia');
+const pega = document.querySelector('#pega');
+let container = document.querySelector('body > div.grid > div.grid-console > div.console');
 
 const mousemove$ = fromEvent<MouseEvent>(document, 'mousemove').pipe(
     throttleTime(200),
@@ -13,8 +18,30 @@ const mousemove$ = fromEvent<MouseEvent>(document, 'mousemove').pipe(
     })
 );
 
+const click$ = fromEvent<MouseEvent>(document, 'click').pipe(
+    scan((count) => count + 1, 0)
+)
+
+const copy$ = fromEvent<ClipboardEvent>(document, "copy").pipe(
+    map((event: ClipboardEvent) => {
+        let clipboardData = event.clipboardData?.getData('text/plain');
+        return {
+            text: clipboardData
+        };
+    })
+);
+
+const paste$ = fromEvent<ClipboardEvent>(document, "paste").pipe(
+    map((event: ClipboardEvent) => {
+        let data = event.clipboardData?.getData('text/plain');
+        return {
+            text: data
+        }
+    })
+);
+
 mousemove$.subscribe({
-    next: (coordinates) => eyeElements.forEach((eye: any) => {
+    next: (coordinates) => {eyeElements.forEach((eye: any) => {
                 const rect = eye.getBoundingClientRect();
 
                 const x = rect.left + rect.width / 2;
@@ -24,7 +51,45 @@ mousemove$.subscribe({
                 const rotation = (radian * (180 / Math.PI) * -1) + 270;
 
                 eye.style.transform = `rotate(${rotation}deg)`;
-            })
-        ,
+
+                if (position) {
+                    position.innerHTML = `<p>X = ${coordinates.x} | Y = ${coordinates.y}</p> `;
+                }
+            })},
     complete: () => console.log("Complete")
 });
+
+click$.subscribe({
+    next: (count) => {
+        if (clicks) {
+            clicks.innerHTML = `<p>${count}</p> `;
+        }
+    }
+})
+
+copy$.subscribe({
+    next: (data) => {
+        let newDiv = document.createElement('div');
+                newDiv.classList.add('message');
+                newDiv.textContent = 'Copiaste: ' + data;
+                container?.insertBefore(newDiv, container.firstChild);
+
+        if (copia) {
+            copia.innerHTML = `<p>${data}</p> `;
+        }
+    },
+    complete: () => {console.log("complete")}
+});
+
+
+paste$.subscribe({
+    next: (data) => {
+        let newDiv = document.createElement('div');
+                newDiv.classList.add('message');
+                newDiv.textContent = 'Pegaste: ' + data.text;
+                container?.insertBefore(newDiv, container.firstChild);
+        console.log(data.text)
+    },
+    complete: () => {console.log("complete")}
+});
+
